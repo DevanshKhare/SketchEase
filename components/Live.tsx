67531bd2f33cee6e0f05ae1a02552736ed448e6b
Devansh Khare <devanshkhare0@gmail.com>
2024-05-04 00:54:28 +0530

    mouse pointer presence setup

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,36 @@
+import { useCallback } from "react";
 import LiveCursors from "./cursor/LiveCursors";
-import { useOthers } from "@/liveblocks.config";
+import { useMyPresence, useOthers } from "@/liveblocks.config";
 
 const Live = () => {
   const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center"
+    >
+      <h1 className="text-2xl text-white">aa</h1>
+
       <LiveCursors others={others} />
     </div>
   );
--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -3,16 +3,19 @@ import Cursor from "./Cursor";
 import { COLORS } from "@/constants";
 
 const LiveCursors = ({ others }: LiveCursorProps) => {
+  {console.log(others)}
   return others.map(({ connectionId, presence }) => {
-    if (!presence) return null;
+    if (!presence?.cursor) return null;
     return (
+      <>
       <Cursor
         key={connectionId}
         color={COLORS[Number(connectionId) % COLORS.length]}
-        x={presence.cursor.x}
-        y={presence.cursor.y}
+        x={presence.cursor?.x}
+        y={presence.cursor?.y}
         message={presence.message}
-      />
+        />
+        </>
     );
   });
 };
