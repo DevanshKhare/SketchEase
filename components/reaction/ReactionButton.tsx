1f28653dc21b2486d83ea0b2e9a75b3021c9cd49
Devansh Khare <devanshkhare0@gmail.com>
2024-05-04 23:47:01 +0530

    initlal flying reaction setup

---
+++ components/reaction/ReactionButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Props = {
+  setReaction: (reaction: string) => void;
+};
+
+export default function ReactionSelector({ setReaction }: Props) {
+  return (
+    <div
+      className="absolute right-0 left-0 bottom-20 mx-auto w-fit transform rounded-full bg-white px-2"
+      onPointerMove={(e) => e.stopPropagation()}
+    >
+      <ReactionButton reaction="👍" onSelect={setReaction} />
+      <ReactionButton reaction="🔥" onSelect={setReaction} />
+      <ReactionButton reaction="😍" onSelect={setReaction} />
+      <ReactionButton reaction="👀" onSelect={setReaction} />
+      <ReactionButton reaction="😱" onSelect={setReaction} />
+      <ReactionButton reaction="🙁" onSelect={setReaction} />
+    </div>
+  );
+}
+
+function ReactionButton({
+  reaction,
+  onSelect,
+}: {
+  reaction: string;
+  onSelect: (reaction: string) => void;
+}) {
+  return (
+    <button
+      className="transform select-none p-2 text-xl transition-transform hover:scale-150 focus:scale-150 focus:outline-none"
+      onPointerDown={() => onSelect(reaction)}
+    >
+      {reaction}
+    </button>
+  );
+}
