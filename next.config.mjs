3e1ad1e75312ae149997eaaeb380dab2f871885c
Devansh Khare <devanshkhare0@gmail.com>
2024-05-05 20:10:41 +0530

    fixed canvas issue

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
       { protocol: "https", hostname: "liveblocks.io", port: "" },
     ],
   },
+  webpack: (config, { webpack }) => {
+    config.externals.push({
+      canvas: "commonjs canvas",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
