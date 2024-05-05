bacc8a6511be528a9afbb67cd35ea6c8e8774019
Devansh Khare <devanshkhare0@gmail.com>
2024-05-06 00:03:19 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,10 +6,10 @@ import { memo } from "react";
 import { navElements } from "@/constants";
 import { ActiveElement, NavbarProps } from "@/types/type";
 
-// import { Button } from "./ui/button";
-// import ShapesMenu from "./ShapesMenu";
+import { Button } from "./ui/button";
+import ShapesMenu from "./ShapesMenu";
 import ActiveUsers from "./users/ActiveUsers";
-// import { NewThread } from "./comments/NewThread";
+import { NewThread } from "./comments/NewThread";
 
 const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps) => {
   const isActive = (value: string | Array<ActiveElement>) =>
@@ -20,7 +20,7 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
 
-      {/* <ul className="flex flex-row">
+      <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
           <li
             key={item.name}
@@ -31,9 +31,9 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
             className={`group px-2.5 py-5 flex justify-center items-center
             ${isActive(item.value) ? "bg-primary-green" : "hover:bg-primary-grey-200"}
             `}
-          > */}
+          >
             {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
-            {/* {Array.isArray(item.value) ? (
+            {Array.isArray(item.value) ? (
               <ShapesMenu
                 item={item}
                 activeElement={activeElement}
@@ -62,10 +62,10 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
                   className={isActive(item.value) ? "invert" : ""}
                 />
               </Button>
-            )} */}
-          {/* </li>
+            )}
+          </li>
         ))}
-      </ul> */}
+      </ul>
 
       <ActiveUsers />
     </nav>
