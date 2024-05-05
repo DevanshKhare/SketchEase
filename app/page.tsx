5a752ab14a909f401030083d5d48a2cf43f1b849
Devansh Khare <devanshkhare0@gmail.com>
2024-05-05 23:57:09 +0530

    added active element logic

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric"
 import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -13,6 +14,17 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object|null>(null);
   const selectedShapeRef = useRef<string|null>('rectangle');
+  const [activeElement, setActiveElement] = useState({
+    name: "",
+    value: "",
+    icon: ""
+  })
+
+  const handleActiveElement = (ele: ActiveElement) => {
+    setActiveElement(ele);
+    selectedShapeRef.current = ele?.value as string;
+  }
+
   useEffect(() => {
     const canvas = initializeFabric({
       canvasRef,
@@ -36,7 +48,7 @@ export default function Page() {
   
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar/>
+      <Navbar activeElement={activeElement} handleActiveElement={handleActiveElement}/>
       <section className="flex h-full flex-row">
         <LeftSidebar/>
         <Live canvasRef={canvasRef}/>
