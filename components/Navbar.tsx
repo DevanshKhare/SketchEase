206e3b53a9df53db243bc55a749cef20f8185630
Devansh Khare <devanshkhare0@gmail.com>
2024-05-09 01:07:53 +0530

    logo changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-[#000000] px-5 text-white">
-      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+      <Image src="/logo.png" alt="FigPro Logo" width={58} height={20} />
 
       <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
