df7b4e5aae7bacfaeac7cf9f5174cfe9938973b6
Devansh Khare <devanshkhare0@gmail.com>
2024-05-10 23:10:10 +0530

    commented code for shape elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,15 +33,17 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
             `}
           >
             {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
-            {Array.isArray(item.value) ? (
-              <ShapesMenu
-                item={item}
-                activeElement={activeElement}
-                imageInputRef={imageInputRef}
-                handleActiveElement={handleActiveElement}
-                handleImageUpload={handleImageUpload}
-              />
-            ) : item?.value === "comments" ? (
+            {
+            // Array.isArray(item.value) ? (
+            //   <ShapesMenu
+            //     item={item}
+            //     activeElement={activeElement}
+            //     imageInputRef={imageInputRef}
+            //     handleActiveElement={handleActiveElement}
+            //     handleImageUpload={handleImageUpload}
+            //   />
+            // ) : 
+            item?.value === "comments" ? (
               // If value is comments, trigger the NewThread component
               <NewThread>
                 <Button className="relative w-5 h-5 object-contain">
