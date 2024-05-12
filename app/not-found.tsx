f38cc4b4435b651c59882f1fc2586757d199aabb
Devansh Khare <devanshkhare0@gmail.com>
2024-05-12 23:57:21 +0530

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='grid place-content-center min-h-screen'>
+    <div className='grid place-content-center min-h-screen bg-black'>
       <h1 className='typing'>404:) Page not found</h1>
     </div>
   )
