df0bcf45e4476bfe80f41bc9c687a39951322085
Devansh Khare <devanshkhare0@gmail.com>
2024-05-13 00:29:05 +0530

    updated room to support dynamic ids

--- app/Room.tsx
+++ app/Room.tsx
@@ -6,9 +6,9 @@ import { ClientSideSuspense } from "@liveblocks/react";
 import { LiveMap } from "@liveblocks/client";
 import Loader from "@/components/Loader";
 
-export function Room({ children }: { children: ReactNode }) {
+export function Room({ children, id }: { children: ReactNode, id: string }) {
   return (
-    <RoomProvider id="my-room" initialPresence={{cursor:null, cursorColor:null, editingText:null}}
+    <RoomProvider id={id} initialPresence={{cursor:null, cursorColor:null, editingText:null}}
       initialStorage={{
         canvasObjects: new LiveMap()
       }}
