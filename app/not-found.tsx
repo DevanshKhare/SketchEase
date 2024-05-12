dec5bd20ba685923f30514909c18273cca304827
Devansh Khare <devanshkhare0@gmail.com>
2024-05-12 23:54:19 +0530

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>404</div>
+    <div className='grid place-content-center min-h-screen'>
+      <h1 className='typing'>404:) Page not found</h1>
+    </div>
   )
 }
 
