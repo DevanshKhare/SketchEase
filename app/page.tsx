817fe6d1a15f3749d8c7ed145f53c677dcc16aa5
Devansh Khare <devanshkhare0@gmail.com>
2024-05-17 22:46:46 +0530

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import MenuBar from '@/components/MenuBar'
 
 const page = () => {
   return (
-    <div>Home page</div>
+    <div className='h-screen pt-8 bg-zinc-950'>
+      <div className='border w-[90%] m-auto border-white h-full'>
+        <header className='h-[5rem] border flex text-white items-center justify-between px-4'>
+            <div>
+              <Image src="/logo.png" alt='logo' height={50} width={50}/>
+            </div>
+            <div>
+              <MenuBar/>
+            </div>
+            <div>
+              <Button variant="outline" className="rounded-[4rem] border-zinc-900 border-[2px] hover:bg-zinc-900">Sign Up</Button>
+            </div>
+        </header>
+      </div>
+    </div>
   )
 }
 
